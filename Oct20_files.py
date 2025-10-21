004ebe4e38544dadd8947beaad8393ce67b46ad8
oliviadonnelly045 <odonnelly@madeira.org>
2025-10-21 09:06:04 -0400

    Update Oct20_files.py
    Explored read and readline methods for files.

--- Oct20_files.py
+++ Oct20_files.py
@@ -1,5 +1,15 @@
 my_schedule = open("schedule.txt")
 
-print(my_schedule)
+#print(my_schedule)
 
-print(my_schedule.read())
+#print(my_schedule.read()) #prints out the entire file, preserves spacing and formatting
+
+#print(my_schedule.read(3)) #if you put a number in the (), prints out that num of characters from the file
+
+
+'''
+print(my_schedule.readline()) #prints a full line of the file
+
+my_schedule.readline() #without printing, just moves the cursor/position in the file
+print(my_schedule.read(3))
+'''
